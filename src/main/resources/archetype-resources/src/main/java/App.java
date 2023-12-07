c66f60f29bc84661108d84ed789acbea2d437069
Vaibhav-kumar20 <152004083+Vaibhav-kumar20@users.noreply.github.com>
2023-12-07 12:28:38 +0530

    Update App.java
    Added some Text

--- src/main/resources/archetype-resources/src/main/java/App.java
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World vaibhav!" );
+        System.out.println( "Hello World vaibhav! , Have a great day" );
     }
 }
